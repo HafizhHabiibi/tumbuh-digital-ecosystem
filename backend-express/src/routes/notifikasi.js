12241efa4da98b605a13703d856d0906c861b939
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-06 11:34:41 +0700

    feat(routes); menambahkan notifikasi.js

---
+++ backend-express/src/routes/notifikasi.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+    getNotifikasi,
+    getBelumDibaca,
+    tandaiDibaca,
+    tandaiSemuaDibaca,
+} from "../controllers/notifikasiController.js";
+import { authenticate } from "../middlewares/auth.js";
+import { authorizeRole } from "../middlewares/role.js";
+
+const router = express.Router();
+
+router.use(authenticate);
+router.use(authorizeRole("orang_tua"));
+
+router.get("/belum-dibaca", getBelumDibaca);
+router.put("/baca-semua", tandaiSemuaDibaca);
+router.get("/", getNotifikasi);
+router.put("/:id/baca", tandaiDibaca);
+
+export default router;

125ff1f5f36fda25498682eade2c82a520427940
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-07 11:56:00 +0700

    feat(routes): dashboard.js

---
+++ backend-express/src/routes/dashboard.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+    getStatistik,
+    getDistribusiGizi,
+    getTrenGizi,
+    getDistribusiRisiko,
+} from "../controllers/dashboardController.js";
+import { authenticate } from "../middlewares/auth.js";
+import { authorizeRole } from "../middlewares/role.js";
+
+const router = express.Router();
+
+router.use(authenticate);
+router.use(authorizeRole("kader", "puskesmas"));
+
+router.get("/statistik", getStatistik);
+router.get("/distribusi", getDistribusiGizi);
+router.get("/tren", getTrenGizi);
+router.get("/risiko", getDistribusiRisiko);
+
+export default router;

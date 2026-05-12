2f9d1ee3586be21cbdc87875c29716575bc4efd7
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-13 00:22:18 +0700

    feat(main.js): menambahkan main.js

--- web-dashboard/src/main.js
+++ web-dashboard/src/main.js
@@ -1,29 +1,33 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-
-import router from "./router";
-
 import { createPinia } from "pinia";
 
 import PrimeVue from "primevue/config";
-import Aura from "@primeuix/themes/aura";
+import Aura from "@primevue/themes/aura";
 
 import VueApexCharts from "vue3-apexcharts";
+import ToastService from "primevue/toastservice";
 
+import router from "./router";
+import App from "./App.vue";
+
+import "./assets/main.css";
 import "primeicons/primeicons.css";
-import "./style.css";
 
 const app = createApp(App);
 
-app.use(router);
 app.use(createPinia());
 
+app.use(router);
+
 app.use(PrimeVue, {
+    ripple: true,
     theme: {
         preset: Aura,
     },
 });
 
-app.use(VueApexCharts);
+app.use(ToastService);
+
+app.component("apexchart", VueApexCharts);
 
 app.mount("#app");

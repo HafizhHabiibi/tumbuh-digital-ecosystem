b552f79f9359812bd01c6fb1e6b7e8a9ca85925f
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-13 00:31:50 +0700

    feat(views): menambahkan template pada semua halaman view

---
+++ web-dashboard/src/views/kader/JadwalView.vue
@@ -0,0 +1,6 @@
+<!-- Contoh placeholder untuk semua view yang belum dibuat -->
+<template>
+    <div class="p-8">
+        <h1 class="text-2xl font-bold">Halaman ini sedang dikerjakan</h1>
+    </div>
+</template>

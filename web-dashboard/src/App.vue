c23d985ef1680d7b40af23fa1c2ef4cc65444469
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-13 00:21:57 +0700

    feat(App.vue): menambahkan App.vue

--- web-dashboard/src/App.vue
+++ web-dashboard/src/App.vue
@@ -1,7 +1,9 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+    <RouterView />
+    <Toast />
 </template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import Toast from "primevue/toast";
+</script>

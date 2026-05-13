1f15c4af95bac663d5ed07abb6432b8b9026931c
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-13 12:57:22 +0700

    refactor(LoginView.vue): ubah struktur code menjadi lebih modular

--- web-dashboard/src/views/auth/LoginView.vue
+++ web-dashboard/src/views/auth/LoginView.vue
@@ -1,6 +1,46 @@
-<!-- Contoh placeholder untuk semua view yang belum dibuat -->
 <template>
-    <div class="p-8">
-        <h1 class="text-2xl font-bold">Halaman ini sedang dikerjakan</h1>
-    </div>
+    <main class="min-h-screen flex flex-col md:flex-row">
+        <LoginBranding />
+
+        <LoginForm
+            v-model:email="form.email"
+            v-model:password="form.password"
+            :is-valid="isFormValid"
+            :loading="authStore.loading.login"
+            :error="authStore.error.login"
+            @submit="handleLogin"
+        />
+    </main>
 </template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/authStore";
+import LoginBranding from "@/components/layout/LoginBranding.vue";
+import LoginForm from "@/components/forms/FormLogin.vue";
+
+const router = useRouter();
+const authStore = useAuthStore();
+
+const form = ref({ email: "", password: "" });
+
+const isFormValid = computed(
+    () => form.value.email.trim() !== "" && form.value.password.length >= 6,
+);
+
+const handleLogin = async () => {
+    if (authStore.loading.login) return;
+
+    const success = await authStore.login(
+        form.value.email,
+        form.value.password,
+    );
+
+    if (success) {
+        router.push({
+            name: authStore.isKader ? "KaderDashboard" : "PuskesmasDashboard",
+        });
+    }
+};
+</script>

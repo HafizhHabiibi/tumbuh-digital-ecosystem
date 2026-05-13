c146cdeb019d8fe836c04db59e1c278107b8457e
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-13 10:18:59 +0700

    feat(layout): menambahkan AppLayout.vue

---
+++ web-dashboard/src/components/layout/AppLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex min-h-screen bg-gray-100">
+        <!-- Sidebar -->
+        <AppSidebar />
+
+        <!-- Content Area -->
+        <div class="flex flex-col flex-1">
+            <!-- Header -->
+            <AppHeader />
+
+            <!-- Page Content -->
+            <main class="flex-1 p-6">
+                <RouterView />
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+
+import AppSidebar from "./AppSidebar.vue";
+import AppHeader from "./AppHeader.vue";
+</script>

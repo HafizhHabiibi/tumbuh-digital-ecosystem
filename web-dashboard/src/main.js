e8e6e9118cf39754584d9fd8543e2536a900dadf
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-13 12:54:37 +0700

    feat(main.js): menambahkan konfigurasi font

--- web-dashboard/src/main.js
+++ web-dashboard/src/main.js
@@ -13,6 +13,11 @@ import App from "./App.vue";
 import "./assets/main.css";
 import "primeicons/primeicons.css";
 
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+
 const app = createApp(App);
 
 app.use(createPinia());

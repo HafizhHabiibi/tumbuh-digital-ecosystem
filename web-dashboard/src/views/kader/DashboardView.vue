a2a7985f580395146d2421784684eb1d24216177
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-05-13 12:57:51 +0700

    feat(DashboardView.vue): menambahkan helper logout

--- web-dashboard/src/views/kader/DashboardView.vue
+++ web-dashboard/src/views/kader/DashboardView.vue
@@ -1,6 +1,20 @@
 <!-- Contoh placeholder untuk semua view yang belum dibuat -->
 <template>
-    <div class="p-8">
+    <div class="p-8 space-y-4">
         <h1 class="text-2xl font-bold">Halaman ini sedang dikerjakan</h1>
+
+        <button
+            @click="resetAuth"
+            class="px-4 py-2 rounded-lg bg-red-500 text-white"
+        >
+            Reset Auth
+        </button>
     </div>
 </template>
+
+<script setup>
+const resetAuth = () => {
+    localStorage.clear();
+    window.location.reload();
+};
+</script>

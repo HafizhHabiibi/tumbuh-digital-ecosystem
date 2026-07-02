122b2d4230a6cba43bed6bde67e8a7eaa6ac4c70
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-07-02 10:36:28 +0700

    feat: implement Firebase Cloud Messaging service and initialize app routing with Riverpod

--- tumbuhapp/lib/main.dart
+++ tumbuhapp/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'router/app_router.dart';
@@ -13,7 +12,6 @@ void main() async {
 
   // Firebase
   await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   await FcmService.initialize();
 
   runApp(

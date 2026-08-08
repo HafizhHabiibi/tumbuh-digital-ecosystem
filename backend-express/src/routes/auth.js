e65488833668837db378013641d5c881284426f2
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-08-09 00:02:55 +0700

    feat: add mobile login route and refresh/logout endpoints to auth routes

--- backend-express/src/routes/auth.js
+++ backend-express/src/routes/auth.js
@@ -1,11 +1,14 @@
 import express from "express";
 import {
     login,
-    loginMobile,
     changePassword,
     forgotPassword,
     resetPassword,
 } from "../controllers/authController.js";
+import {
+    refreshAccessToken,
+    revokeRefreshToken,
+} from "../controllers/refreshTokenController.js";
 
 import { authenticate } from "../middlewares/auth.js";
 import { ipRateLimit, emailRateLimit } from "../middlewares/loginRateLimit.js";
@@ -13,9 +16,10 @@ import { ipRateLimit, emailRateLimit } from "../middlewares/loginRateLimit.js";
 const router = express.Router();
 
 router.post("/login", ipRateLimit, emailRateLimit, login);
-router.post("/login/mobile", ipRateLimit, emailRateLimit, loginMobile);
 router.post("/forgot-password", ipRateLimit, forgotPassword);
 router.post("/reset-password", ipRateLimit, resetPassword);
 router.put("/change-password", authenticate, changePassword);
+router.post("/refresh", refreshAccessToken);
+router.post("/logout", authenticate, revokeRefreshToken);
 
 export default router;
